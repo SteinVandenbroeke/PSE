2d06468af32d48222529bc348e573ba1642de8d1
Stein Vandenbroeke <steinvandenbroeke@gmail.com>
2021-04-28 13:03:21 +0200

    tags check

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -46,6 +46,7 @@ void Simulation::importXmlFile(const char *path, const char *knownTagsPad, std::
     REQUIRE(!FileIsEmpty(path), "The file that needs to be read must not be empty");
 
     XMLReader xmlReader = XMLReader(path);
+    xmlReader.acceptedTags(errorStream, knownTagsPad);
     this->fcentra = xmlReader.readVaccinationCenters(errorStream);
     this->fhub = xmlReader.readHubs(this->fcentra, errorStream);
 
